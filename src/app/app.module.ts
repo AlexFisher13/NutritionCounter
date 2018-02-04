2676da788b1d4c1bf78c41dc6c1127f323babd46
AlexFisher13 <verji13>
2018-02-04 12:33:02 +0300

    add counting ration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { UserDataComponent } from './user-data/user-data.component';
-import { UserResultComponent } from './user-result/user-result.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDataComponent,
-    UserResultComponent
+    UserDataComponent
   ],
   imports: [
     BrowserModule
--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -5,7 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user-data.component.html',
   styleUrls: ['./user-data.component.css']
 })
+
 export class UserDataComponent implements OnInit {
+
+  ration;
+  protein;
+  fat;
+  carbo;
+  isShow = false;
+
+  printData(height, weight) {
+    this.isShow = true;
+    this.ration = Math.ceil(Number(weight.value) * 30);
+    this.protein = Math.ceil(Number(weight.value) * 1.7);
+    this.fat = Math.ceil(Number(weight.value) * 1.2);
+    this.carbo = Math.ceil(Number(weight.value) * 3.1);
+    return false;
+  }
 
   constructor() { }
 
